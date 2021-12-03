3112b586ef969e4f84763753aef6366132cf8086
Brian Addicks <brian@addicks.us>
2021-12-03 16:08:03 -0500

    updated set-cwmproject

--- CwManage/Public/CwmProject/Set-CwmProject.ps1
+++ CwManage/Public/CwmProject/Set-CwmProject.ps1
@@ -1,56 +1,31 @@
 function Set-CwmProject {
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory = $False, Position = 0, ValueFromPipeline = $True)]
-        [CwmProject]$CwmProject
+        [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+        [CwmProject]$CwmProject,
+
+        [Parameter(Mandatory = $True)]
+        [string]$Body
     )
 
     BEGIN {
         $VerbosePrefix = "Set-CwmProject:"
 
         $ApiParams = @{}
-        $ApiParams.UriPath = 'project/projects'
+        $ApiParams.Method = 'PATCH'
+        $ApiParams.Body = $Body
     }
 
     PROCESS {
         $CurrentProject = Get-CwmProject -ProjectId $CwmProject.ProjectId
         if (-not $CurrentProject) {
             Throw 'Project does not exist, this cmdlet currently only support modifying projects'
+        } else {
+            $ApiParams.UriPath = 'project/projects/' + $CwmProject.ProjectId
         }
     }
 
     END {
+        Invoke-CwmApiQuery @ApiParams
     }
 }
-
-
- <#
-     $Uri = "https://api-na.myconnectwise.net/"
-    $Uri += 'v4_6_Release/apis/3.0/'
-    $Uri += "service/tickets"
-
-    $ApiParams = @{}
-    $ApiParams.Uri = $Uri
-    $ApiParams.AuthString = $AuthString
-    $ApiParams.Method = 'Post'
-    $ApiParams.Body = @{}
-
-    $WhatIfMessage = "`r`n"
-    $WhatIfMessage += "Updating Service Ticket`r`n"
-
-    $ReturnObject = @()
-    foreach ($config in $ConfigurationId) {
-        $ApiParams.Body.Id = $config
-        foreach ($ticket in $TicketNumber) {
-            $ApiParams.Uri += '/' + $ticket + '/configurations'
-            $WhatIfMessage += "Uri: $Uri`r`n"
-            $WhatIfMessage += "Body: $($ApiParams.Body | ConvertTo-Json)`r`n"
-            if ($PSCmdlet.ShouldProcess($WhatIfMessage)) {
-                $ReturnValue = Invoke-CwmApiCall @ApiParams
-            }
-            $ReturnObject += $ReturnValue
-        }
-    }
-
-    $ReturnObject
-    #>
